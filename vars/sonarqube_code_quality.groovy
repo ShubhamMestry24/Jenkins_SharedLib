d80f9d80425298d59076654b2b14ecde47e7ec65
ShubhamMestry24 <86059404+ShubhamMestry24@users.noreply.github.com>
2025-09-19 20:26:37 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -2,6 +2,6 @@ def call(){
   timeout(time: 1, unit: "MINUTES"){
       waitForQualityGate abortPipeline: false
   /*timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: true
+      waitForQualityGate abortPipeline: true*/
   }
 }
